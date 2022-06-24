07eb311846e3f51f753193a460300182d8cc3ecf
iamtee <demiladeleshi@gmail.com>
2022-06-24 04:30:22 +0100

    save my repository

---
+++ intro.js
@@ -0,0 +1,7 @@
+let myName = " Leshi Taiwo";
+let myHeight = " 1.53m";
+let myCountry =" Nigeria";
+
+console.log("My name is" + myName);
+console.log("I am" + myHeight +" tall");
+console.log("I am from" + myCountry);
