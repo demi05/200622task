a0a67108136092b527da3b71bca60a566dd22b87
Leshi Taiwo <98643006+demi05@users.noreply.github.com>
2022-06-24 20:41:23 +0100

    Update intro.js

--- intro.js
+++ intro.js
@@ -1,7 +1,4 @@
-let myName = " Leshi Taiwo";
-let myHeight = " 1.53m";
-let myCountry =" Nigeria";
 
-console.log("My name is" + myName);
-console.log("I am" + myHeight +" tall");
-console.log("I am from" + myCountry);
+alert('My name is Leshi Taiwo');
+alert ('I am 1.53m tall');
+alert ('I am from Nigeria');
